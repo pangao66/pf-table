e6130fad254ca219cf70b7e0c0001908cf4e2c17
pangao <pangao@tuhu.cn>
2019-12-11 10:27:33 +0800

    deploy

--- assets/js/20.db771333.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{253:function(n,w,o){}}]);
---
+++ assets/js/22.7b272ecc.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{265:function(n,w,o){}}]);
